fe28a12b781df332eae399ebbc8401fc3b216f24
iamjink <jinijinjin@gmail.com>
2020-01-12 18:14:38 -0800

    setting css styles and setting up variables in javascript

---
+++ assets/javascript/games.js
@@ -0,0 +1,13 @@
+
+//letter choices
+var userChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//randomizing letter choices that user can type from userChoices array. math.floor() returns largest element less than or equal to in array
+var computerChoices = userChoices[Math.floor(Math.random() * userChoices.length)]; 
+
+//
+var win = 0;
+var losses = 0;
+var guessCountdown = 0;
+var userGuess = 0;
+
