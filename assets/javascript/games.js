8228db82d113aeb67b6a15fe469f028d570b23c4
iamjink <jinijinjin@gmail.com>
2020-01-12 18:25:07 -0800

    fixing url of image bg

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -5,9 +5,13 @@ var userChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm
 //randomizing letter choices that user can type from userChoices array. math.floor() returns largest element less than or equal to in array
 var computerChoices = userChoices[Math.floor(Math.random() * userChoices.length)]; 
 
-//
+//setting each variable to start
 var win = 0;
 var losses = 0;
-var guessCountdown = 0;
-var userGuess = 0;
+var guessCountdown = 9;
+var userGuess = [];
 
+//defining each function and changing text in the scoreBoard div
+function GuessCount () {
+    document.getElementById("#guessCountdown").innerHTML = "Guesses Left" + guessesLeft;
+}
